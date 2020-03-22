b0e2606e27c17cb483ead727548f1ed6b6038d79
ricardomarzochi@gmail.com <r.libera.marzochi@accenture.com>
2020-03-22 12:15:33 +0100

    NewVersion-Develop/01

--- src/main/java/com/packsendme/api/tolls/repository/ITolls_Repository.java
+++ src/main/java/com/packsendme/api/tolls/repository/ITolls_Repository.java
@@ -5,6 +5,7 @@ import org.springframework.data.mongodb.repository.Query;
 
 public interface ITolls_Repository extends MongoRepository<TollsCountry_Model, String> {
 	
-	@Query("{'name_country' : ?0}")
+	//@Query("{'name_country' : ?0}")
+	@Query("{'name_country' :  {$eq: ?0}}")
 	TollsCountry_Model findCostTollByNameContry(String name_country);
 }
